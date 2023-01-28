98dec3d831ce1455e2394c48d0c72a64c2bd6fe6
cytoplasma <jpiromg@gmail.com>
2023-01-28 03:10:50 -0800

    fixed config, models folder, and .env folder for database connection

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,26 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
