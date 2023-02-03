b785ce54215c6ed7c51f811f17f92a5582c7e6c7
cytoplasma <jpiromg@gmail.com>
2023-02-03 09:17:22 -0800

    fixed delete route, readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# ecommerce
+# E-commerce Back End
+
+## Description 
+
+This application provides a framework for the ability to access database information using sequelize. In this application, you may freely add to, update, create, or remove from database tables. 
+
+## Table of Contents 
+
+* [Installation](#installation) 
+
+* [Usage](#Usage) 
+
+* [License](#license) 
+
+## Installation 
+
+First clone the code from github repository to your local machine. Then run ```npm i``` to install all the packages needed to run the application.
+
+Next, you will have to connect to your database, create the tables and seed your db. 
+1. To create the db tables, log in to mysql through mysql shell and run ```source db/schema``` to create the tables.
+2. Connect to database through sequelize. You can do so by simply creating a '.env' file with key-value pairs containing your mysql information like below. 
+    ```
+    JAWSDB_URL = 'mysql:<user>:<password>@127.0.0.1/ecommerce_db'
+    DB_NAME = 'ecommerce_db'
+    DB_USER = <user>
+    DB_PASSWORD = <password>
+    ```
+3. Seed your databse by running ```npm run seed```
+
+Then you should end up with a seeded database that is ready for connection.
+
+## Usage 
+
+Though this application is not yet ready for public usage, you can test out its functionaility through Insomna. 
+
+As you can see from the demonstration video, you can use get, put, post, and delete methods on each of the db tables, and changes should be reflected when you return to the '/' route for each api route. 
+
+Note that once a category is deleted, all products under that category will be removed as well. 
+
+## License 
+
+MIT
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,53 +3,54 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  Tag.findAll({
-    include: [
-      {
-        model: Product,
-        through: ProductTag,
-      },
-    ],
-  })
-    .then((tags) => res.status(200).json(tags))
-    .catch((err) => res.status(500).json(err));
+router.get('/', async(req, res) => {
+  // find all tags, including its associated Product data
+  try {
+    const tags = await Tag.findAll({ include: Product });
+    return res.json(tags)
+  } catch(err) {
+    res.status(500).json(err);
+  } 
 });
 
-router.get('/:id', (req, res) => {
-  Tag.findOne({
-    where: {
-      id: req.params.id,
-    },
-    include: [
-      {
-        model: Product,
-        through: ProductTag,
-      },
-    ],
-  })
-    .then((tag) => res.status(200).json(tag))
-    .catch((err) => res.status(404).json(err));
+router.get('/:id', async(req, res) => {
+  // find a single tag by its `id`, including its associated Product data
+  try {
+    const tag = await Tag.findByPk(req.params.id, { include: Product });
+    return res.json(tag);
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  Tag.create(req.body)
-    .then((tag) => res.status(200).json(tag))
-    .catch((err) => res.status(404).json(err));
+router.post('/', async(req, res) => {
+  // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    return res.json(newTag);
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  Tag.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((tag) => res.status(200).json(tag))
-    .catch((err) => res.status(404).json(err));
+router.put('/:id', async(req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, { where: { id: req.params.id }});
+    return res.json(updateTag);
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({ where: { id: req.params.id}});
+    return res.json(deleteTag);
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
